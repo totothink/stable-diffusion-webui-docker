11b084c2004a289ad3d23589a0c13e558c4c2629
张亚龙 <696029200@qq.com>
2024-07-20 03:47:18 +0800

    内置ComfyUI-Manager并确保新安装插件存放在容器外部

--- services/comfy/entrypoint.sh
+++ services/comfy/entrypoint.sh
@@ -10,6 +10,9 @@ MOUNTS["/root/.cache"]="/data/.cache"
 MOUNTS["${ROOT}/input"]="/data/config/comfy/input"
 MOUNTS["${ROOT}/output"]="/output/comfy"
 
+# custom_nodes映射到/data/config/comfy/custom_nodes，支持Comfyui-Manager自动下载的保存在外挂目录，避免因容器删除而导致数据丢失
+MOUNTS["${ROOT}/custom_nodes"]="/data/config/comfy/custom_nodes"
+
 for to_path in "${!MOUNTS[@]}"; do
   set -Eeuo pipefail
   from_path="${MOUNTS[${to_path}]}"
